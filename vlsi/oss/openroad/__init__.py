a71afa2feca1c239d3f2e72379fea242e5771f6f
oronpo <oron.port@cornell.edu>
2022-11-06 18:21:51 -0500

    add openroad

---
+++ vlsi/oss/openroad/__init__.py
@@ -0,0 +1,29 @@
+from dfr_scripts.common import GitOSSTool
+
+class SpecificTool(GitOSSTool):
+  def __init__(self, version: str):
+    super().__init__("vlsi", "openroad", version, "https://github.com/The-OpenROAD-Project/OpenROAD")
+  def buildAndInstallShellCmd(self, flags: str) -> str:
+    return f"""
+      git submodule update --init --recursive
+      mkdir -p build
+      cd build
+      cmake .. -DCMAKE_INSTALL_PREFIX={self.installPath()}
+      make -j`nproc`  
+      make install
+    """
+  def env_path(self) -> list[str]:
+    return [
+      f"{self.linkedPath()}/{self.execFolder()}",
+      f"{self.linkedPath()}/{self.execFolder()}/Linux-x86_64",
+      f"{self.linkedPath()}/pdn/scripts",
+    ]
+  def env_man_path(self) -> list[str]:
+    return [f"{self.installPath()}/share/man"]
+  def env_ld_library_path(self) -> list[str]:
+    return [
+      f"{self.installPath()}/lib",
+      f"{self.installPath()}/lib/Linux-x86_64",
+    ]
+  def env_extra(self) -> dict[str, str]:
+    return {"OPENROAD" : self.installPath()}
