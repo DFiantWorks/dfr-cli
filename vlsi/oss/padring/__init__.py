37fc7a60ab355c03e8b92bbe81b34cbd2a22d50b
oronpo <oron.port@cornell.edu>
2022-11-05 22:56:25 -0400

    add padring support

---
+++ vlsi/oss/padring/__init__.py
@@ -0,0 +1,14 @@
+from dfr_scripts.common import GitOSSTool
+
+class SpecificTool(GitOSSTool):
+  def __init__(self, version: str):
+    super().__init__("vlsi", "padring", version, "https://github.com/donn/padring")
+  def buildAndInstallShellCmd(self, flags: str) -> str:
+    return f"""
+      bash ./bootstrap.sh 
+      cd build/ 
+      ninja
+      mkdir -p {self.installPath()}/bin 
+      cp padring {self.installPath()}/bin/
+    """
+
