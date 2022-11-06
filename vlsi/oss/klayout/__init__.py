da454051f4313a4f1969b08509d5cc7718841b3c
oronpo <oron.port@cornell.edu>
2022-11-06 13:39:55 -0500

    add klayout

---
+++ vlsi/oss/klayout/__init__.py
@@ -0,0 +1,14 @@
+from dfr_scripts.common import GitOSSTool
+
+#TODO: figure out why the openlane docker installation of klayout is so different
+class SpecificTool(GitOSSTool):
+  def __init__(self, version: str):
+    super().__init__("vlsi", "klayout", version, "https://github.com/KLayout/klayout")
+  def buildAndInstallShellCmd(self, flags: str) -> str:
+    return f"""
+      mkdir -p {self.installPath()}
+      ./build.sh -j`nproc` -prefix {self.installPath()} -without-qtbinding
+    """
+  def execFolder(self) -> str:
+    return ""
+
